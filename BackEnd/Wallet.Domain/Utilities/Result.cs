b8a6561f11dcced52d763aab8f59c94a2290e5ee
João Eduardo <joaoeduardoj920@gmail.com>
2025-05-07 20:56:49 -0300

    feat:Adicionando o padrão result

---
+++ BackEnd/Wallet.Domain/Utilities/Result.cs
@@ -0,0 +1,65 @@
+﻿namespace Wallet.Domain.Utilities
+{
+    public class Result : ResultBase
+    {
+
+        public Result(params string[] errors) : base(errors) { }
+        public Result(IEnumerable<string> errors) : base(errors) {}
+
+        public static Result<TValue> FromValue<TValue>(TValue value)
+        {
+            return new Result<TValue>(value);
+        }
+
+        public static Result<TValue> FromError<TValue>(params string[] errors)
+        {
+            return new Result<TValue>(errors);
+        }
+
+        public static Result<TValue> FromError<TValue>(IEnumerable<string> errors)
+        {
+            return new Result<TValue>(errors);
+        }
+
+        public static Result FromError(params string[] errors)
+        {
+            return new Result(errors);
+        }
+
+        public static Result FromSuccess()
+        {
+            return new Result();
+        }
+    }
+
+    public class Result<TValue> : Result
+    {
+        public TValue Value { get; set; }
+
+        public Result(params string[] errors) : base(errors) { }
+        public Result(IEnumerable<string> errors) : base(errors) { }
+        public Result(List<string> errors) : base(errors) { }
+
+
+        public Result(TValue value)
+        {
+            Value = value;
+        }
+
+        public static Result<TValue> FromValue(TValue value)
+        {
+            return new Result<TValue>(value);
+        }
+
+        public static new Result<TValue> FromErrors(params string[] errors)
+        {
+            return new Result<TValue>(errors);
+        }
+
+        public static new Result<TValue> FromErrors(IEnumerable<string> errors)
+        {
+            return new Result<TValue>(errors);
+        }
+
+    }
+}
---
+++ BackEnd/Wallet.Domain/Utilities/ResultBase.cs
@@ -0,0 +1,29 @@
+﻿namespace Wallet.Domain.Utilities
+{
+    public class ResultBase
+    {
+        public IEnumerable<string> Errors { get; set; }
+        public bool Success => Errors.Any();
+
+        protected ResultBase(params string[] errors) 
+        { 
+            Errors = errors.ToList();
+        }
+
+        protected ResultBase(IEnumerable<string> errors)
+        {
+            Errors = errors;
+        }
+
+        public ResultBase() 
+        {
+            Errors = new List<string>();
+        }
+
+        public void AddError(params string[] errors)
+        {
+            Errors = Errors.Concat(errors);
+        }
+        
+    }
+}

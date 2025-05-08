ded3d386edbf30f0bcf922d574a090c397009bc1
João Eduardo <joaoeduardoj920@gmail.com>
2025-05-08 17:04:22 -0300

    feat: ajuste header

--- FrontEnd/src/app/layout.tsx
+++ FrontEnd/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/hearder";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,10 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="flex flex-col h-full">
-          <Header />
-          {children}
-        </div>
+        <SessionWrapper>
+          <div className="flex flex-col h-full">
+            <Header />
+            {children}
+          </div>
+        </SessionWrapper>
       </body>
     </html>
   );
--- FrontEnd/src/components/hearder/index.tsx
+++ FrontEnd/src/components/hearder/index.tsx
@@ -1,11 +1,27 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
+
 export const Header = () => {
+  const { data: session } = useSession();
+
   return (
     <header className="bg-[linear-gradient(90deg,_rgb(255,1,150)_0%,_rgb(124,51,185)_100%)] text-white flex items-center justify-between p-4">
       <h1 className="text-xl font-bold">Carteira</h1>
       <section className="flex gap-5 items-center">
-        <p className="text-lg  italic">
-          Transferencias mais rápidas que o pix !
+        <p className={`text-lg ${session ? "" : "italic"}`}>
+          {session
+            ? `Bem-vindo, ${session.user?.name} !`
+            : "Transferências mais rápidas que o Pix!"}
         </p>
+        {session && (
+          <button
+            onClick={() => signOut({ callbackUrl: "/" })}
+            className="text-lg cursor-pointer font-medium hover:text-gray-200"
+          >
+            Sair
+          </button>
+        )}
       </section>
     </header>
   );
